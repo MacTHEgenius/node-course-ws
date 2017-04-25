66bc6e6d909286aa526771dabac3a003ca51b386
Benoit Levesque <genius.blevesque@gmail.com>
2017-04-25 14:54:13 +0000

    Use describe to group tests

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,18 +3,26 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => expect(res.body).toInclude({ error: 'Page not found.' }))
-        .end(done);
-});
-
-it('should return my user object', (done) =>{
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => expect(res.body).toInclude({ name: 'mtg', age: 20 }))
-        .end(done);
+describe('Server', () => {
+    
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => expect(res.body).toInclude({ error: 'Page not found.' }))
+                .end(done);
+        });
+    });
+    
+    describe('GET /users', () => {
+        it('should return my user object', (done) =>{
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => expect(res.body).toInclude({ name: 'mtg', age: 20 }))
+                .end(done);
+        });
+    });
+    
 });
